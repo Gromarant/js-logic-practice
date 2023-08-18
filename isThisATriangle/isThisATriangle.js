b50db5b9480e6597c6f9eaf2e29d00288ff0ed23
Mariangelica Rodriguez <gromarant@gmail.com>
2023-08-18 10:24:49 +0200

    add/secondApproach_and_logsToEveryApproach

--- isThisATriangle/isThisATriangle.js
+++ isThisATriangle/isThisATriangle.js
@@ -8,3 +8,10 @@ const isTriangle = (a,b,c) =>  {
     return false;
   }
 };
+console.log(isTriangle(1,2,2));
+console.log(isTriangle(7,2,2));
+
+//second approach
+const isTriangle2 = (a,b,c) => a + b > c && a + c > b && c + b > a ? true :  false;
+console.log(isTriangle2(1,2,2));
+console.log(isTriangle2(7,2,2));
