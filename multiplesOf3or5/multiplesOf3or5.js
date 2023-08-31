25dfcbd0ac03f10d37e9693eb50d7f03c1384f17
Mariangelica Rodriguez <gromarant@gmail.com>
2023-08-31 12:37:38 +0200

    solve/multiplesOf3Or5_exercise_secondApproach

--- multiplesOf3or5/multiplesOf3or5.js
+++ multiplesOf3or5/multiplesOf3or5.js
@@ -9,3 +9,13 @@ function solution(number) {
   return sum;
 };
 console.log(solution(10));
+
+//second approach
+const solution2 = (num) => {
+  let sum = 0;
+  for (let i= num-1; i > 0; i--) {
+    ( i % 5 === 0 || i % 3 === 0 )? sum += i : sum;
+  }
+  return sum;
+}
+console.log(solution2(10))
